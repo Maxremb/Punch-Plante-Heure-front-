40e7e713f73b1c737688651ced0cb888e1b2c505
jmmathevet <jmmathevet@gmail.com>
2020-04-01 14:59:29 +0200

    modification de detail jardin

---
+++ src/app/content/jardin/detail-jardin/detail-jardin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-jardin',
+  templateUrl: './detail-jardin.component.html',
+  styleUrls: ['./detail-jardin.component.css']
+})
+export class DetailJardinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
