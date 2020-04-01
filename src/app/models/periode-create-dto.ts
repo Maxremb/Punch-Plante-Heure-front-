d950b84bfc8dce630ba4ba0fa785acb471d30e88
FlavienGomila <flavien.gomila@gmail.com>
2020-04-01 14:38:23 +0200

    Ajout département au models periode

--- src/app/models/periode-create-dto.ts
+++ src/app/models/periode-create-dto.ts
@@ -1,7 +1,9 @@
 import { PeriodeEnum } from "../enums/periode-enum.enum";
+import { DepartementUpdateDto } from './departement-update-dto';
 
 export class PeriodeCreateDto {
     dateDebut: Date;
     dateFin: Date;
     type: PeriodeEnum;
+    departement: DepartementUpdateDto;
 }
