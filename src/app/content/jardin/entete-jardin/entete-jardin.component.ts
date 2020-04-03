82a07ea147b23089e50c55ca89a16d195b727c22
unknown <maxime.rembert@gmail.com>
2020-04-03 10:00:55 +0200

    padding left style jardin + entete jardin component

---
+++ src/app/content/jardin/entete-jardin/entete-jardin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entete-jardin',
+  templateUrl: './entete-jardin.component.html',
+  styleUrls: ['./entete-jardin.component.css']
+})
+export class EnteteJardinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
