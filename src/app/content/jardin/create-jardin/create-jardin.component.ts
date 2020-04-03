69984deaa048b78b53f93eae992153296d23ba82
unknown <maxime.rembert@gmail.com>
2020-04-03 12:40:40 +0200

    navbar 15%

--- src/app/content/jardin/create-jardin/create-jardin.component.ts
+++ src/app/content/jardin/create-jardin/create-jardin.component.ts
@@ -4,6 +4,7 @@ import { JardinCreateDto } from 'src/app/models/jardin-create-dto';
 import {JardinService} from 'src/app/services/jardin-service.service';
 import { UtilisateurUpdateDto} from 'src/app/models/utilisateur-update-dto';
 import { DepartementCreateDto } from 'src/app/models/departement-create-dto';
+import { DepartementService } from 'src/app/services/departement.service';
 
 
 @Component({
@@ -19,11 +20,15 @@ export class CreateJardinComponent implements OnInit {
   messageValidation = null;
   messageErreur = null;
   utilisateurActif = new UtilisateurUpdateDto;
-  allDepartements = new DepartementCreateDto;
+  allDepartements = new Array<DepartementCreateDto>();
 
-  constructor(private service : JardinService) { }
+  constructor(
+    private service : JardinService,
+    private deptService : DepartementService,
+    ) { }
 
   ngOnInit(): void {
+    this.getAllDept();
     // Affecter l'user actif au jardin
     //this.utilisateurActif = this.service.utilisateurActif
     this.addJardinForm = new FormGroup({
@@ -32,7 +37,7 @@ export class CreateJardinComponent implements OnInit {
       "length": new FormControl(this.jardin.length),
       "width": new FormControl(this.jardin.width),
       "dept": new FormControl(this.jardin.dept, Validators.required),
-      "user" : new FormControl(this.jardin.user = this.utilisateurActif, Validators.required),
+      "user" : new FormControl(this.jardin.user = this.utilisateurActif),
     })
   }
 
@@ -50,6 +55,14 @@ export class CreateJardinComponent implements OnInit {
           this.messageValidation = responseDto.message;
         } else { this.messageErreur = responseDto.message; }
        }
+    )
+  }
+
+  getAllDept() {
+    this.deptService.getAll().subscribe(
+      responseDto => {
+        if (!responseDto.error) {this.allDepartements = responseDto.body;}
+      }
     )
   }
 }
