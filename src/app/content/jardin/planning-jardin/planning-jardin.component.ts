c796e9bf256b1d9220d0c61a042c030b73b64202
IsalineMllt <isalinemillet38@gmail.com>
2020-04-08 19:42:01 +0200

    planning fonctionnel

--- src/app/content/jardin/planning-jardin/planning-jardin.component.ts
+++ src/app/content/jardin/planning-jardin/planning-jardin.component.ts
@@ -3,7 +3,6 @@ import { PlanteUtilisateurUpdateDto } from 'src/app/models/plante-utilisateur-up
 import { PeriodeService } from 'src/app/services/periode.service';
 import { PlanteUtilisateurService } from 'src/app/services/plante-utilisateur-service.service';
 import { ActivatedRoute } from '@angular/router';
-import { PeriodeEnum } from 'src/app/enums/periode-enum.enum';
 
 @Component({
   selector: 'app-planning-jardin',
@@ -14,24 +13,18 @@ export class PlanningJardinComponent implements OnInit {
 
   allPlantes = new Array<PlanteUtilisateurUpdateDto>();
 
-
   constructor(
-    // private jardinservice: JardinService,
     private periodeService: PeriodeService,
     private planteUtilisateurService: PlanteUtilisateurService,
     private route: ActivatedRoute,
   ) { }
 
-
   ngOnInit(): void {
     this.getPlantes();
-
-
   }
 
   getPlantes(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-
     this.planteUtilisateurService.getAllByJardin(id, 0).subscribe(
       (responseDto) => {
         if (!responseDto.error) {
@@ -43,7 +36,10 @@ export class PlanningJardinComponent implements OnInit {
   }
 
   getPeriodes(): void {
-    this.allPlantes[0].rempotage = [];
+    for (let p = 0; p < this.allPlantes.length; p++) {
+
+
+    this.allPlantes[p].rempotage = [];
     this.allPlantes.forEach(plante =>
       this.periodeService.getByDepAndPlanteModelIdAndType(
         plante.garden.dept.depNum,
@@ -51,9 +47,7 @@ export class PlanningJardinComponent implements OnInit {
         'REMPOTAGE')
         .subscribe(
           (responseDto) => {
-
             if (!responseDto.error) {
-
               for (let i = 1; i < 13; i++) {
                 if (Number.parseInt(responseDto.body.startDate.toString().split("-")[1]) > i || Number.parseInt(responseDto.body.endDate.toString().split("-")[1]) < i) {
                   plante.rempotage.push("neutre");
@@ -61,13 +55,95 @@ export class PlanningJardinComponent implements OnInit {
                   plante.rempotage.push("rempotage");
                 }
               }
-              console.log(plante.rempotage);
+            }
+          }
+        )
+    );
+    
+    this.allPlantes[p].floraison = [];
+    this.allPlantes.forEach(plante =>
+      this.periodeService.getByDepAndPlanteModelIdAndType(
+        plante.garden.dept.depNum,
+        plante.modelPlant.identifiant,
+        'FLORAISON')
+        .subscribe(
+          (responseDto) => {
+            if (!responseDto.error) {
+              for (let i = 1; i < 13; i++) {
+                if (Number.parseInt(responseDto.body.startDate.toString().split("-")[1]) > i || Number.parseInt(responseDto.body.endDate.toString().split("-")[1]) < i) {
+                  plante.floraison.push("neutre");
+                } else {
+                  plante.floraison.push("floraison");
+                }
+              }
+            }
+          }
+        )
+    )
 
+    this.allPlantes[p].semis = [];
+    this.allPlantes.forEach(plante =>
+      this.periodeService.getByDepAndPlanteModelIdAndType(
+        plante.garden.dept.depNum,
+        plante.modelPlant.identifiant,
+        'SEMIS')
+        .subscribe(
+          (responseDto) => {
+            if (!responseDto.error) {
+              for (let i = 1; i < 13; i++) {
+                if (Number.parseInt(responseDto.body.startDate.toString().split("-")[1]) > i || Number.parseInt(responseDto.body.endDate.toString().split("-")[1]) < i) {
+                  plante.semis.push("neutre");
+                } else {
+                  plante.semis.push("semis");
+                }
+              }
+            }
+          }
+        )
+    )
+
+    this.allPlantes[p].fructification = [];
+    this.allPlantes.forEach(plante =>
+      this.periodeService.getByDepAndPlanteModelIdAndType(
+        plante.garden.dept.depNum,
+        plante.modelPlant.identifiant,
+        'FRUCTIFICATION')
+        .subscribe(
+          (responseDto) => {
+            if (!responseDto.error) {
+              for (let i = 1; i < 13; i++) {
+                if (Number.parseInt(responseDto.body.startDate.toString().split("-")[1]) > i || Number.parseInt(responseDto.body.endDate.toString().split("-")[1]) < i) {
+                  plante.fructification.push("neutre");
+                } else {
+                  plante.fructification.push("fructification");
+                }
+              }
+            }
+          }
+        )
+    )
+
+    this.allPlantes[p].taille = [];
+    this.allPlantes.forEach(plante =>
+      this.periodeService.getByDepAndPlanteModelIdAndType(
+        plante.garden.dept.depNum,
+        plante.modelPlant.identifiant,
+        'TAILLE')
+        .subscribe(
+          (responseDto) => {
+            if (!responseDto.error) {
+              for (let i = 1; i < 13; i++) {
+                if (Number.parseInt(responseDto.body.startDate.toString().split("-")[1]) > i || Number.parseInt(responseDto.body.endDate.toString().split("-")[1]) < i) {
+                  plante.taille.push("neutre");
+                } else {
+                  plante.taille.push("taille");
+                }
+              }
             }
           }
         )
     )
   }
 
-
+}
 }
