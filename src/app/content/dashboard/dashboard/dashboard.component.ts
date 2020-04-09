5d2bf23b168a5025babdd5fa9607b92a25a0fd1d
jmmathevet <jmmathevet@gmail.com>
2020-04-09 18:20:30 +0200

    new modif pour dashboard

--- src/app/content/dashboard/dashboard/dashboard.component.ts
+++ src/app/content/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilisateurUpdateDto } from 'src/app/models/utilisateur-update-dto';
+import { JardinService } from 'src/app/services/jardin-service.service'
+import { JardinUpdateDto } from 'src/app/models/jardin-update-dto';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  utilisateur: UtilisateurUpdateDto;
+  allJardins = new Array<JardinUpdateDto>();
+
+  constructor(private jardinservice: JardinService, ) { }
 
   ngOnInit(): void {
+    this.getAllJardins();
   }
 
+  getAllJardins() {
+    this.jardinservice.getAllByUtilisateur(this.utilisateur.identifier, 0).subscribe(
+      responseDto => {
+          if (!responseDto.error) {
+            this.allJardins = responseDto.body.content;
+          }
+        },
+      responseDtoerror => {
+          if (responseDtoerror.error) {
+            this.allJardins = [];
+          }
+        
+        }
+    );
+  }
 }
