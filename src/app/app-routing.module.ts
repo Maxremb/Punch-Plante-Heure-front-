10d1e6a1c3c0529b6949a87cdc427b005d549d4b
CloarecTM <tm.cloarec@gmail.com>
2020-04-10 16:23:04 +0200

    related to gestion Admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,6 +36,8 @@ import { ConnexionComponent } from './content/connexion/connexion.component';
 import { InscriptionComponent } from './content/inscription/inscription/inscription.component';
 import { MeteoComponent } from './content/meteo/meteo/meteo.component';
 import { DetailUtilisateurComponent } from './content/dashboard/dashboard/detail-utilisateur/detail-utilisateur.component';
+import { GestionAdminComponent } from './content/utilisateur/gestion-admin/gestion-admin.component'
+
 
 
 const routes: Routes = [
@@ -44,7 +46,7 @@ const routes: Routes = [
   { path : 'admin' , component :  DashboardAdminComponent },
   { path : 'jardin' , component :  AllJardinComponent},
   { path : 'jardin/create' , component : CreateJardinComponent },
-  
+
   { path : 'plant/vadmin', component : AllPlantComponent },
   { path : 'plant/create', component : CreatePlantComponent },
   { path : 'plant/update/:id', component : DetailedPlantComponent },
@@ -71,6 +73,7 @@ const routes: Routes = [
 
   { path : 'inscription', component : InscriptionComponent},
   { path : 'utilisateur/detail', component : DetailUtilisateurComponent},
+  { path : 'utilisateur/gestion-admin', component : GestionAdminComponent},
 
   { path : 'jardin/detail/:id', component : DetailJardinComponent }, //detail d'un jardin par id
   { path : 'jardin/update/:id', component : UpdateJardinComponent }, //update d'un jardin par id
--- src/app/content/utilisateur/gestion-admin/gestion-admin.component.ts
+++ src/app/content/utilisateur/gestion-admin/gestion-admin.component.ts
@@ -86,6 +86,31 @@ export class GestionAdminComponent implements OnInit {
       );
     }
 
+    desactivateUser(identifier: number) {
+      this.service.desactivateUser(identifier).subscribe(
+        responseDto => {
+          console.log('debug responseDto : ', responseDto);
+          if (!responseDto.error) {
+            this.allUtilisateur = this.allUtilisateur.filter(
+              element =>  element.identifier !== identifier
+            );
+          }
+        }
+      );
+    }
+
+    activateUser(identifier: number) {
+      this.service.activateUser(identifier).subscribe(
+        responseDto => {
+          console.log('debug responseDto : ', responseDto);
+          if (!responseDto.error) {
+            this.allUtilisateur = this.allUtilisateur.filter(
+              element =>  element.identifier !== identifier
+            );
+          }
+        }
+      );
+    }
 
 
 
