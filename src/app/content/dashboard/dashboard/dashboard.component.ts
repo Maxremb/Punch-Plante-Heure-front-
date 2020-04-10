747adee428610fa0b96214ab1103b8feeb9d5807
jmmathevet <jmmathevet@gmail.com>
2020-04-10 15:33:10 +0200

    new dashboard

--- src/app/content/dashboard/dashboard/dashboard.component.ts
+++ src/app/content/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UtilisateurUpdateDto } from 'src/app/models/utilisateur-update-dto';
+import { ConnectedUser } from 'src/app/models/connectedUser';
 
 
 @Component({
@@ -9,14 +10,30 @@ import { UtilisateurUpdateDto } from 'src/app/models/utilisateur-update-dto';
 })
 export class DashboardComponent implements OnInit {
 
-  utilisateur: UtilisateurUpdateDto;
+  user: ConnectedUser;
+  utilisateur: UtilisateurUpdateDto = new UtilisateurUpdateDto();
   
-
   constructor( ) { }
 
   ngOnInit(): void {
-    
+    this.user = JSON.parse(localStorage.getItem('connectedUser'));
+    //this.getUtilisateur();
   }
 
+ // getUtilisateur() {
+  //  this.service.getId(this.user.id).subscribe(
+  //    (responseDto) => {
+   //         if (!responseDto.error) {
+    //          this.utilisateur = this.responseDto.body;
+  //          }
+   //       }
+   //   );
+    
+ // }
+
+  desactiver(id: number) {
+    this.utilisateur.active = false;
+    
+  }
   
 }
